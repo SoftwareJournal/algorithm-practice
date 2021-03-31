cefb44dcf712f4b0a72af803a020d744bbdf0edb
unknown <mekkah.apps@gmail.com>
2021-03-31 14:56:45 -0500

    Used dynamic array to implement Singly Linkedlist

--- LinkedList/DesignLinkedList.java
+++ LinkedList/DesignLinkedList.java
@@ -1,10 +1,14 @@
 package LinkedList;
 
-class MyLinkedList {
+import java.util.ArrayList;
+import java.util.List;
+
+//First attempt, just need to revisit this implemenation
+class MyLinkedList1 {
     int length;
     Node head;
     /** Initialize your data structure here. */
-    public MyLinkedList() {
+    public MyLinkedList1() {
         head = new Node(0);
         head.next = null;
         length = 0;
@@ -114,3 +118,46 @@ class Node{
  * obj.addAtIndex(index,val);
  * obj.deleteAtIndex(index);
  */
+
+ //Second attempt using dynamic list/array; accepted
+ class MyLinkedList {
+    List<Integer> node;
+    /** Initialize your data structure here. */
+    public MyLinkedList() {
+       node = new ArrayList<>();
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        if(index < 0 || index >= node.size()){
+            return -1;
+        }
+        
+        return node.get(index);        
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+        node.add(0,val);
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        node.add(val);
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if(index < 0 || index > node.size()){
+            return;
+        }
+        node.add(index,val);
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        if (index >= 0 && index < node.size()) {
+            node.remove(index);
+        }
+    }
+}
