b280c18a0a57df87c55086bc07d34f1e6866c59a
unknown <mekkah.apps@gmail.com>
2021-04-02 09:26:41 -0500

    Solved 46/54 cases for Design Circular Queue problem

--- Queue/DesignCircularQueue.java
+++ Queue/DesignCircularQueue.java
@@ -1,11 +1,12 @@
 package Queue;
 /**
+ * 46 / 54 test cases passed.
  * Need to solve for this test case:
- * ["MyCircularQueue","enQueue","enQueue","enQueue","enQueue","deQueue","deQueue","isEmpty","isEmpty","Rear","Rear","deQueue"]
-[[8],[3],[9],[5],[0],[],[],[],[],[],[],[]]
+ * ["MyCircularQueue","enQueue","Front","isFull","enQueue","enQueue","enQueue","deQueue","enQueue","enQueue","isEmpty","Rear"]
+[[4],[3],[],[],[7],[2],[5],[],[4],[2],[],[]]
 
-Output: [null,true,true,true,true,true,true,false,false,0,0,false]
-Expected: [null,true,true,true,true,true,true,false,false,0,0,true]
+Output: [null,true,3,false,true,true,true,true,true,true,false,2]
+Expec.: [null,true,3,false,true,true,true,true,true,false,false,4]
  */
 class MyCircularQueue {
     int[] queue;
@@ -46,23 +47,22 @@ class MyCircularQueue {
     
     //take out the first element
     public boolean deQueue() {
-        if(isEmpty()){
-            p_end = -1;
-            p_start = -1;
+        if(isEmpty() == true){
             return false;
         }
-        if(p_start == 0 && p_end == 0){
+        
+        if(p_start == 0 && p_end == 0 || p_start == p_end){
             p_end = -1;
             p_start = -1;        
             return true;
         }
         //before the end of the array max index
-        else if(!isEmpty() && p_start+1 < p_end){
+        else if(isEmpty() == false && p_start+1 <= p_end){
             p_start++;
             return true;
         }
         // pass the end of the array max index
-        else if(!isEmpty() && p_start+1 > len-1){
+        else if(isEmpty() == false && p_start+1 > len-1){
             p_start = 0;
             return true;
         }
@@ -92,15 +92,15 @@ class MyCircularQueue {
         }
         return false;
     }
+    
     //is the queue full
     public boolean isFull() {
         // if p_end after p_start
-        int end_pos = p_end+1;
-        if(end_pos - p_start > len-1 && p_start <= end_pos){
+        if(p_end - p_start + 2 > len && p_start < p_end){
             return true;
         }
         // if p_end before p_start
-        else if((len - 1 - p_start) + end_pos > len && end_pos <= p_start){
+        else if(len - 1 - p_start + p_end + 2 > len && p_end < p_start){
             return true;
         }
         
