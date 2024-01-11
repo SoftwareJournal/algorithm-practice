4560240152ec8a9ed8701a2bde63ccd4a3402fbe
unknown <mekkah.apps@gmail.com>
2024-01-10 20:59:15 -0600

    Update to telephone check validator

--- freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Cert-Projects/TelephoneNumberValidator.js
+++ freeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Cert-Projects/TelephoneNumberValidator.js
@@ -1,8 +1,10 @@
 function telephoneCheck(str) {
-  return /1*\s*([0-9]{3})-*\s*[0-9]{3}-*\s*[0-9]{4}/.test(str);
+  return /^1*\s*([0-9]{3})-*\s*[0-9]{3}-*\s*[0-9]{4}$/.test(str);
 }
-let number = "1 456 789 4444";
+let number = "1 (2555) 456 4582";
 let isValidNumber = telephoneCheck(number);
 console.log(isValidNumber);
+
+
   
   
